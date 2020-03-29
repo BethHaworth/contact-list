7ddd8602bbf33fec98c1c1f0e800252bb4967519
BethHaworth <beth.haworth@focusrite.com>
2020-03-29 15:23:34 +0100

    add material ui

--- contact-list/src/App.js
+++ contact-list/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Contacts from './components/Contacts';
+import Paginate from './components/Paginate';
+import Typography from '@material-ui/core/Typography';
 import './App.css';
+import { Container } from '@material-ui/core';
+
 
 class App extends Component {
 
@@ -19,7 +24,11 @@ class App extends Component {
 
   render() {
     return (
+      <Container maxWidth="lg"> 
+      <Typography variant="h2" className="title"> Contact List </Typography>
       <Contacts contacts={this.state.contacts} />
+      <Paginate />
+      </Container>
     );
   }
 }
--- contact-list/src/components/Contacts.js
+++ contact-list/src/components/Contacts.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Avatars from './Avatars';
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import './Contacts.css';
 
-    const Contacts = ({ contacts }) => {
-      return (
-        <div>
-          <center><h1>Contact List</h1></center>
-          {contacts.map((contact) => (
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">{contact.name}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">{contact.email}</h6>
-                <p class="card-text">{contact.company.catchPhrase}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      )
-    };
+const Contacts = ({ contacts }) => {
+    return (
+        <Card raised="true" className="card"> 
+                <div>
+                {contacts.map((contact) => (
+                    <div class="card">
+                    <div class="card-body">
+                    <CardContent>
+                        <Avatars />
+                        <Typography variant="h2" class="card-title">{contact.name}</Typography>
+                        <Typography variant="h4" class="card-subtitle mb-2 text-muted">{contact.email}</Typography>
+                        <Typography variant="p3" class="card-text">{contact.company.catchPhrase}</Typography>
+                    </CardContent>
+                    <hr />
+                    </div>
+                    </div>
+                ))}
+                </div>
+        </Card>
+    )
+}
 
-    export default Contacts
+export default Contacts;
